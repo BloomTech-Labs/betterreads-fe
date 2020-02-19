37b9b1e0a6f2f5ce25c12ff11d63e8aa4f387b9f
Mr. Jonah <jaitch@gmail.com>
2020-02-19 16:44:06 -0600

    redux logger added and enabled for ReduxDevTools

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
+import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 import { reducer } from './reducers/Reducer';
 import App from './App';
 import './index.css';
 
-const store = createStore(reducer, applyMiddleware(thunk));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk, logger)));
 
 ReactDOM.render(
 	<Provider store={store}>
