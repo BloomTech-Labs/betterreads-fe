bee133d78241f063f0ede220d7b24198a68f5e9a
Miguel Nicolas <miguelqnicolas@gmail.com>
2020-02-19 20:20:43 -0500

    sign up and sign in pages completed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import LandingPage from './components/LandingPage';
+import SignUp from './components/SignUp';
+import SignIn from './components/SignIn';
 
 const App = () => {
 	return (
 		<Router>
 			<Route exact path="/" component={LandingPage} />
-			{/* <Route path="/signup" component={LandingPage} />
-			<Route path="/signin" component={LandingPage} />
-			<Route path="/dashboard" component={LandingPage} />
+			<Route path="/signup" component={SignUp} />
+			<Route path="/signin" component={SignIn} />
+			{/* <Route path="/dashboard" component={LandingPage} />
 			<Route path="/mylibrary" component={LandingPage} /> */}
 		</Router>
 	);
