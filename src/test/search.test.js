1187bbad8f12786ba4afe957a51acd3bc18fee6a
Mr. Jonah <jaitch@gmail.com>
2020-02-21 17:32:37 -0600

    Added enzyme for react testing

--- src/test/search.test.js
+++ src/test/search.test.js
@@ -1,1 +1,7 @@
-// Test test
+import React from 'react';
+import { render } from '@testing-library/react';
+import Search from '../components/search/Search';
+
+test('Search is rendering', () => {
+    render(<Search />);
+})
