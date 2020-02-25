4c091c42c2721b294d41d746dd49709ee38aaa40
Miguel <miguelqnicolas@gmail.com>
2020-02-25 16:35:07 -0500

    making api call for all of user's books

--- src/components/library/Library.js
+++ src/components/library/Library.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import axios from 'axios';
 import Search from '../search/Search';
 import styled from 'styled-components';
@@ -6,6 +6,18 @@ import styled from 'styled-components';
 const LibraryContainer = styled.div``;
 
 const Library = props => {
+	useEffect(() => {
+		axios
+			.get(
+				`http://localhost:5000/api/${localStorage.getItem(
+					'user_id'
+				)}/library`,
+				{ withCredentials: true }
+			)
+			.then(response => console.log(response))
+			.catch(error => console.log(error));
+	}, []);
+
 	const signOut = () => {
 		axios
 			.get('http://localhost:5000/api/auth/signout', {
@@ -29,6 +41,9 @@ const Library = props => {
 			<Search />
 			<h2>My Shelves</h2>
 			<p>Create shelves and add books to your custom shelves.</p>
+			<p>To be read (0)</p>
+			<p>In progress (0)</p>
+			<p>Finished (0)</p>
 		</>
 	);
 };
