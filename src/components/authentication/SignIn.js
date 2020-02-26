019be5a4c729b2ea3fa5c03b3043a53b603e46e2
Miguel <miguelqnicolas@gmail.com>
2020-02-26 12:07:41 -0500

    lightly styled sign up and sign in components

--- src/components/authentication/SignIn.js
+++ src/components/authentication/SignIn.js
@@ -1,17 +1,64 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import  ReactGA from 'react-ga';
+import ReactGA from 'react-ga';
 import styled from 'styled-components';
 
 const SignInContainer = styled.div`
 	width: 90%;
 	margin: 0 auto;
+	margin-top: 64px;
+	margin-bottom: 64px;
 	display: flex;
 	flex-direction: column;
+
+	h2 {
+		margin-bottom: 32px;
+	}
 
 	form {
 		display: flex;
 		flex-direction: column;
+
+		label {
+			font-size: 1rem;
+		}
+
+		input {
+			margin-bottom: 16px;
+			padding: 12px;
+			border: 1px solid gray;
+			border-radius: 3px;
+			font-family: 'Nunito', sans-serif;
+			font-size: 1rem;
+		}
+
+		.error {
+			margin-top: -8px;
+			font-size: 0.875rem;
+			color: red;
+		}
+
+		button {
+			margin-top: 16px;
+			margin-bottom: 8px;
+			padding: 12px;
+			border: none;
+			border-radius: 3px;
+			font-family: 'Nunito', sans-serif;
+			font-size: 1rem;
+		}
+	}
+
+	.need {
+		margin-bottom: 16px;
+		font-size: 0.875rem;
+		text-align: center;
+	}
+
+	.or {
+		margin-bottom: 16px;
+		font-size: 1rem;
+		text-align: center;
 	}
 
 	a {
@@ -76,9 +123,9 @@ const SignIn = props => {
 	const [error, setError] = useState('');
 
 	useEffect(() => {
-		ReactGA.event({ category: 'Sign In', action: 'Sign in loaded'})
+		ReactGA.event({ category: 'Sign In', action: 'Sign in loaded' });
 		ReactGA.pageview(window.location.pathname + window.location.search);
-	}, [])
+	}, []);
 
 	const onChange = event => {
 		setInput({
@@ -113,6 +160,7 @@ const SignIn = props => {
 				<label htmlFor="emailAddress">Email Address</label>
 				<input
 					type="email"
+					placeholder="Enter your email"
 					name="emailAddress"
 					value={input.emailAddress}
 					onChange={onChange}
@@ -122,6 +170,7 @@ const SignIn = props => {
 				<label htmlFor="password">Password</label>
 				<input
 					type="password"
+					placeholder="Enter your password"
 					name="password"
 					value={input.password}
 					onChange={onChange}
@@ -129,16 +178,16 @@ const SignIn = props => {
 					minLength="5"
 				/>
 
-				{error && <p>{error}</p>}
+				{error && <p className="error">{error}</p>}
 
 				<button type="submit">Sign in</button>
 			</form>
 
-			<p onClick={() => props.history.push('/signup')}>
+			<p className="need" onClick={() => props.history.push('/signup')}>
 				Need an account? Sign up here.
 			</p>
 
-			<p>OR</p>
+			<p className="or">OR</p>
 
 			<a href="http://localhost:5000/api/auth/google">
 				<button className="google-button">
--- src/components/authentication/SignUp.js
+++ src/components/authentication/SignUp.js
@@ -5,12 +5,59 @@ import styled from 'styled-components';
 const SignUpContainer = styled.div`
 	width: 90%;
 	margin: 0 auto;
+	margin-top: 64px;
+	margin-bottom: 64px;
 	display: flex;
 	flex-direction: column;
+
+	h1 {
+		margin-bottom: 32px;
+	}
 
 	form {
 		display: flex;
 		flex-direction: column;
+
+		label {
+			font-size: 1rem;
+		}
+
+		input {
+			margin-bottom: 16px;
+			padding: 12px;
+			border: 1px solid gray;
+			border-radius: 3px;
+			font-family: 'Nunito', sans-serif;
+			font-size: 1rem;
+		}
+
+		.error {
+			margin-top: -8px;
+			font-size: 0.875rem;
+			color: red;
+		}
+
+		button {
+			margin-top: 16px;
+			margin-bottom: 8px;
+			padding: 12px;
+			border: none;
+			border-radius: 3px;
+			font-family: 'Nunito', sans-serif;
+			font-size: 1rem;
+		}
+	}
+
+	.already {
+		margin-bottom: 16px;
+		font-size: 0.875rem;
+		text-align: center;
+	}
+
+	.or {
+		margin-bottom: 16px;
+		font-size: 1rem;
+		text-align: center;
 	}
 
 	a {
@@ -122,6 +169,7 @@ const SignUp = props => {
 				<label htmlFor="fullName">Full Name</label>
 				<input
 					type="text"
+					placeholder="Enter your name"
 					name="fullName"
 					value={input.fullName}
 					onChange={onChange}
@@ -131,6 +179,7 @@ const SignUp = props => {
 				<label htmlFor="emailAddress">Email Address</label>
 				<input
 					type="email"
+					placeholder="Enter your email"
 					name="emailAddress"
 					value={input.emailAddress}
 					onChange={onChange}
@@ -140,6 +189,7 @@ const SignUp = props => {
 				<label htmlFor="username">Username</label>
 				<input
 					type="text"
+					placeholder="Enter a username"
 					name="username"
 					value={input.username}
 					onChange={onChange}
@@ -150,6 +200,7 @@ const SignUp = props => {
 				<label htmlFor="password">Password</label>
 				<input
 					type="password"
+					placeholder="Enter a password"
 					name="password"
 					value={input.password}
 					onChange={onChange}
@@ -160,6 +211,7 @@ const SignUp = props => {
 				<label htmlFor="confirmPassword">Confirm Password</label>
 				<input
 					type="password"
+					placeholder="Reenter your password"
 					name="confirmPassword"
 					value={input.confirmPassword}
 					onChange={onChange}
@@ -167,16 +219,19 @@ const SignUp = props => {
 					minLength="5"
 				/>
 
-				{error && <p>{error}</p>}
+				{error && <p className="error">{error}</p>}
 
 				<button type="submit">Create account</button>
 			</form>
 
-			<p onClick={() => props.history.push('/signin')}>
+			<p
+				className="already"
+				onClick={() => props.history.push('/signin')}
+			>
 				Already have an account? Sign in here.
 			</p>
 
-			<p>OR</p>
+			<p className="or">OR</p>
 
 			<a href="http://localhost:5000/api/auth/google">
 				<button className="google-button">
