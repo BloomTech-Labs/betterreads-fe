d62b1abfb4b9586a996f07323a2fe37272be8ed2
Mr. Jonah <jaitch@gmail.com>
2020-03-02 19:22:48 -0600

    Breadcrumbs and chelf notes

--- src/components/common/BookList.js
+++ src/components/common/BookList.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import BookItem from './BookItems';
+import Breadcrumbs from './Breadcrumbs';
+import ShelfNote from './ShelfNote';
 
 const BookList = (props) => {
     return (
         <>
+            <ShelfNote type="search" query={props.query || ''} count={props.count} />
             {
                 props.bookList && 
                 props.bookList.map(book => (
+                    <>
                     <BookItem key={book.id} book={book} />
+                    </>
                 ))
             }
         </>
