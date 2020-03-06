8f5fada30682fdcb3a7ef88c908b45e5b9994f43
Miguel <miguelqnicolas@gmail.com>
2020-03-05 21:30:17 -0500

    styling for desktop library complete

--- src/components/library/Library.js
+++ src/components/library/Library.js
@@ -1,10 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
-import {
-	fetchUsersBooks,
-	fetchUsersShelves,
-	getGoogleResults
-} from '../../actions/index';
+import { fetchUsersBooks, fetchUsersShelves, getGoogleResults } from '../../actions/index';
 import Header from '../common/Header';
 import SearchForm from '../search/SearchForm';
 import LibraryContainer from './LibraryStyle';
@@ -27,76 +23,57 @@ const Library = props => {
 	return (
 		<LibraryContainer>
 			<Header history={props.history} />
-			<div className="what-are-you-reading">
-				<div className="what-are-you-reading-container">
+
+			<div className="what-are-you-reading-container">
+				<div className="what-are-you-reading">
 					<h2>What are you reading?</h2>
-					<p>
-						Search for a book that you want to track and add to
-						shelves.
-					</p>
+					<p>Search for a book that you want to track and add to shelves.</p>
 				</div>
 				<SearchForm history={props.history} />
 			</div>
 
-			<div className="reading-status">
+			<div className='reading-status-and-my-shelves-container'>
 				<div className="reading-status-container">
-					<p>To be read ({toBeRead.length})</p>
-					<p>In progress ({inProgress.length})</p>
-					<p>Finished ({finished.length})</p>
-				</div>
-			</div>
-
-			<div className="my-shelves">
-				<h2>My Shelves</h2>
-				<p className="create-shelves">
-					Create shelves and add books to your custom shelves.
-				</p>
-
-				{/* <button className="create-new-shelf-button">
-					Create new shelf
-				</button> */}
-
-				<div className="shelves-container">
-					<div
-						className="shelf"
-						onClick={() => props.history.push('/shelf/allbooks')}
-					>
-						<p className="shelf-name">All books</p>
-						<BookIcon height="40px" width="40px" fill="#D9D9D9" />
-						{props.userBooks.length === 1 ? (
-							<p className="shelf-quantity">1 book</p>
-						) : (
-							<p className="shelf-quantity">
-								{props.userBooks.length} books
-							</p>
-						)}
+					<div className="reading-status">
+						<p>To be read ({toBeRead.length})</p>
+						<p>In progress ({inProgress.length})</p>
+						<p>Finished ({finished.length})</p>
 					</div>
+				</div>
 
-					{/* <div className="shelf">
-						<p className="shelf-name">Favorites</p>
-						<BookIcon height="40px" width="40px" fill="#D9D9D9" />
-						<p className="shelf-quantity">0 books</p>
-					</div> */}
+				<div className="my-shelves">
+					<h2>My Shelves</h2>
+					<p className="create-shelves">Create shelves and add books to your custom shelf.</p>
+					<button className="create-new-shelf-button">Create new shelf</button>
 
-					{/* {props.userShelves.map(item => {
-						return (
-							<div
-								className="shelf"
-								onClick={() =>
-									props.history.push(`/shelf/${item.id}`)
-								}
-							>
-								<BookIcon
-									height="64px"
-									width="64px"
-									fill="#E5E5E6"
-								/>
-								<p className="shelf-name">{item.shelfName}</p>
-								<p className="shelf-quantity">0 books</p>
-							</div>
-						);
-					})} */}
-					{/* not doing anything with private value yet, waiting on design */}
+					<div className="shelves-container">
+						<div className="shelf" onClick={() => props.history.push('/shelf/allbooks')}>
+							<p className="shelf-name">All books</p>
+							<BookIcon height="40px" width="40px" fill="#d9d9d9" />
+							{props.userBooks.length === 1 ? (
+								<p className="shelf-quantity">1 book</p>
+							) : (
+								<p className="shelf-quantity">{props.userBooks.length} books</p>
+							)}
+						</div>
+
+						<div className="shelf">
+							<p className="shelf-name">Favorites</p>
+							<BookIcon height="40px" width="40px" fill="#D9D9D9" />
+							<p className="shelf-quantity">0 books</p>
+						</div>
+
+						{/* {props.userShelves.map(item => {
+							return (
+								<div className="shelf" onClick={() => props.history.push(`/shelf/${item.id}`)}>
+									<BookIcon height="64px" width="64px" fill="#E5E5E6" />
+									<p className="shelf-name">{item.shelfName}</p>
+									<p className="shelf-quantity">0 books</p>
+								</div>
+							);
+						})}
+						not doing anything with private value yet, waiting on design */}
+					</div>
 				</div>
 			</div>
 		</LibraryContainer>
---
+++ src/components/library/ShelfItem.js
@@ -0,0 +1,229 @@
+import React, { useState, useEffect, useRef } from "react";
+import { connect } from 'react-redux';
+import { saveBookToLibrary } from '../../actions'
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import { Button, Rate, Menu, Dropdown, notification } from 'antd';
+import HeartOutlined from '@ant-design/icons/HeartOutlined';
+import HeartFilled from '@ant-design/icons/HeartFilled';
+import DownOutlined from '@ant-design/icons/DownOutlined';
+import { Event } from '../tracking/';
+
+const Wrapper = styled.div`
+    .ant-dropdown-menu {
+        z-index: 1001;
+    }
+
+    .betterReadsOrange {background: #D24719;}
+    .betterReadsGreen{background-color: #547862;}
+
+    .frank{font-family: 'Frank Ruhl Libre', serif;}
+    .openSans{font-family: 'Open Sans', sans-serif;}
+    
+    .fs-13{font-size: 13px;}
+    .fs-14{font-size: 14px;}
+    .fs-16{font-size: 16px;}
+
+    .fw-600{font-weight: 600;}
+    .fw-bold{font-wieght: bold;} 
+
+    .lh-20{line-height: 20px;}
+    .lh-22{line-height: 22px;}
+
+    .flexer{
+        display: flex;
+        border-bottom: 1px solid #cecece;
+        padding: 16px 0;
+
+        &:first-child {
+            
+        }
+
+        .anticon-heart svg{
+            height: 26px;
+            width: 29px;
+            color: #D24719;
+        }
+
+        .imgContainer{
+            margin-right: 16px; 
+            
+            .thumbContainer{
+                width: 125px;
+                height: 198px;
+                overflow: hidden;
+                
+                .smallThumbnail {
+                    width: 135px;
+                    height: auto;
+                }
+            }
+            
+            .ant-btn {
+                color: #F7F7F7;
+                width: 82px;
+                border: none;
+                border-radius: 0 0 0 5px;
+                font-size: 13px;
+                font-weight: 600;
+                line-height: 20px;
+                padding: 0 3px;
+        
+                .anticon-down{
+                    margin-left: 2px;
+                }
+
+                svg {
+                    margin-right: 4px;
+                }
+            }
+        }
+    
+        .bookDetail {
+            display: flex;
+            flex-direction: column;
+            
+            .bookTitle{
+                line-height: 22px
+                margin-bottom: 12px;
+            }
+
+            .ant-select-selection {   
+                background-color: rgba(0,0,0,0);
+            }
+
+            .bookRating{
+                .anticon-star svg {
+                    height: 16px;
+                    width: 16px;
+                }
+            }
+        }
+
+        .bookFav {
+            margin-left: auto;
+        }
+    }
+
+    @media (min-width: 1120px) {
+        width: 45%;
+        margin: 0 18px 0 0;
+    }
+`;
+
+const ThumbContainer = styled.div`
+    border-radius: 5px 0 0;
+    height: 95px;
+    width: 82px;
+    background-image: url(${props => props.bgImage});
+    background-size: cover;
+`;
+
+const BookItem = props => {
+    const [favorite, setFavorite] = useState(false);
+    const [readingStatus, setReadingStatus] = useState();
+    const [trackBtnLabel, setTrackBtnLabel] = useState('Track this');
+
+    // favorite status
+    const firstRun = useRef(true);
+
+    useEffect(() => {
+        if(firstRun.current){
+            firstRun.current = false;
+            return;
+        }
+
+        Event(
+			'Search',
+			(favorite ? 'User added a book to favorites from search list.' : 'User removed a book from favorites on search list.' ),
+			'SEARCH_RESULT'
+		);        
+
+        notification.open({
+            type: (favorite ? 'success' : 'info'),
+            message: 'Success',
+            description: (favorite ? 'Book added to favorites.' : 'Book removed from favorites.'),
+            duration: 1.5
+        });
+
+        props.saveBookToLibrary(localStorage.getItem('id'), props.book.id, props.book, 1, favorite);
+    }, [favorite])
+
+    // reading status
+    const firstRunStatus = useRef(true);
+
+    useEffect(() => {
+        if(firstRunStatus.current){
+            firstRunStatus.current = false;
+            return;
+        }
+        
+        Event(
+			'Search',
+			'User added a book with a reading status',
+			'SEARCH_RESULT'
+        );
+
+        props.saveBookToLibrary(localStorage.getItem('id'), props.book.id, props.book, readingStatus, null);
+    }, [readingStatus])
+
+    const readingStatusUpdate = key => {
+        setReadingStatus(key.item.props.value);
+        setTrackBtnLabel(key.item.props.children);
+        notification.open({
+            type: 'info',
+            message: 'Success',
+            description: 'You are now tracking a book',
+            duration: 1.5
+        });
+        
+    }
+
+    const TrackMenu = (
+        <Menu onClick={key => readingStatusUpdate(key)}>
+            <Menu.Item key="80" value="1">To be read</Menu.Item>
+            <Menu.Item key="71" value="2">In progress</Menu.Item>
+            <Menu.Item key="62" value="3">Finished</Menu.Item>
+        </Menu>
+    )
+    
+    return (
+        <Wrapper>
+            <div className="flexer">
+                <div className="imgContainer">
+                    {props.book && (
+                        <Link to={`/book/${props.book.id}`} onClick={() => Event('Book', 'User clicked for book details', 'SEARCH_RESULTS')}>
+                            <ThumbContainer bgImage={props.book.thumbnail} />
+                        </Link>
+                    )}
+                    <Dropdown overlay={TrackMenu} trigger={['click']}>
+                        <Button className={(trackBtnLabel === 'Track this' ? 'betterReadsOrange' : 'betterReadsGreen')}>{trackBtnLabel} <DownOutlined /></Button>
+                    </Dropdown>
+                </div>
+                <div className="bookDetail openSans">
+                    <div className="bookTitle fs-16 fw-600">{props.book.title}</div>
+                    <div className="bookAuthors fs-16 openSans lh-22">
+                        {
+                            props.book &&
+                            props.book.authors.split(',').map((author, index) => (
+                                <div key={index} data-key={index}>
+                                    { index === 0 && 'by' } {author}
+                                </div>
+                            ))
+                        }
+                    </div>
+                </div>
+                <div className="bookFav">
+                    {   favorite
+                        ? <HeartFilled onClick={() => setFavorite(!favorite)} /> 
+                        : <HeartOutlined onClick={() => setFavorite(!favorite)} />
+                    }
+                    
+                </div>
+            </div>
+        </Wrapper>
+    );
+
+};
+
+export default connect(null, {saveBookToLibrary})(BookItem);
