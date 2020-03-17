bfd7b396f8aecb77ceccb15cda6ee8fed2a64bbe
Miguel <miguelqnicolas@gmail.com>
2020-03-17 11:28:49 -0400

    modified authentication reducer and imports and exports

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,7 @@
 import { combineReducers } from 'redux';
 import library from './library';
 import search from './search';
-import authentication from './authentication';
+import { reducer as authentication } from './authentication';
 import book from './book'
 
 export default combineReducers({
