f1ec174d91366da3e00e4d15efa471bc22883ad2
Miguel <miguelqnicolas@gmail.com>
2020-03-18 12:22:52 -0400

    started working on redux thunk testing with redux mock store

--- src/tests/authentication/SignIn.test.js
+++ src/tests/authentication/SignIn.test.js
@@ -4,11 +4,15 @@ import { createMemoryHistory } from 'history';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
+import { configureStore } from 'redux-mock-store';
+import { signIn } from '../../actions';
 import reducer from '../../reducers';
 import { render, fireEvent, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import App from '../../App';
 import SignIn from '../../components/authentication/SignIn';
+
+const mockStore = configureStore([thunk]);
 
 const renderWithRedux = (component, { initialState, store = createStore(reducer, initialState, applyMiddleware(thunk)) } = {}) => {
 	return {
@@ -35,3 +39,18 @@ test('redirects to the sign up component', () => {
     const signUpHeading = getByTestId('sign-up-heading');
     expect(signUpHeading).toBeInTheDocument();
 });
+
+// test('error displays', () => {
+//     const { debug, getByTestId }  = renderWithRedux(<SignIn />);
+//     const store = mockStore({});
+
+//     return store.dispatch(signIn({ emailAddress: 'thisemailaddress@doesnotexist', password: 'thisisthewrongpassword' }))
+//         .then(() => {
+//             const actions = store.getActions()
+//             expect(actions[0]).toEqual(success())
+//         })
+    
+//     debug();
+
+//     // create a mock store that resembles our reducer
+// });
