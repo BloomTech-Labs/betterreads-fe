9931e3b616dcd7b43db3b3bf5a18058ea01de69d
Miguel <miguelqnicolas@gmail.com>
2020-03-19 16:43:03 -0400

    adjusted the bookcardlist component for the search pagination component

--- src/components/common/BookCardList.js
+++ src/components/common/BookCardList.js
@@ -1,31 +1,35 @@
 import React, { useEffect } from 'react';
 import BookCard from './BookCard';
+import SearchPagination from '../search/SearchPagination';
 import styled from 'styled-components';
-
-// things to be passed, history, book array, source, width
 
 const BookCardListContainer = styled.div`
     width: 90%;
     margin: 0 auto;
 
     @media(min-width: 1120px) {
-        max-width: 687px;
-        width: 100%;
+        width: 687px;
         margin: 0;
-        display: flex;
-        justify-content: space-between;
-        align-content: flex-start;
-        flex-wrap: wrap;
+
+        .book-card-list {
+            display: flex;
+            justify-content: space-between;
+            align-content: flex-start;
+            flex-wrap: wrap;
+        }
     }
 `;
 
 const BookCardList = props => {
     return (
-        <BookCardListContainer>
-            {props.books.map((item, index) => (
-                <BookCard key={index} history={props.history} book={item} source={props.source} />
-            ))}
-        </BookCardListContainer>
+        <>
+            <BookCardListContainer>
+                <div className='book-card-list'>
+                    {props.books.map((item, index) => <BookCard key={index} history={props.history} book={item} source={props.source} />)}
+                </div>
+                {props.source === 'search' && <SearchPagination />}
+            </BookCardListContainer>
+        </>
     );
 };
 
