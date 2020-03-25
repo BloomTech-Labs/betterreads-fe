ee91f86fd1c8f4101b9aaebf442d91fe5a035545
Mr. Jonah <jaitch@gmail.com>
2020-03-25 15:50:22 -0500

    Loader component added and implemented on search

---
+++ src/components/common/Loader.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Spin } from 'antd';
+import { LoadingOutlined } from '@ant-design/icons';
+
+const LoaderContainer = styled.div`
+    width: 90%;
+    margin: auto;
+    height: 25vh;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    align-items: center;
+
+    & > div {
+        font-size: 1.5rem;
+        font-family: 'Frank Ruhl Libre', sans-serif;
+    }
+
+    @media(min-width: 1120px) {
+        width: 687px;
+        margin: 0;
+    }
+`;
+
+
+const Loader = (props) => {
+    console.log(props)
+
+    const antIcon = <LoadingOutlined style={{ fontSize: props.size || '24px', color: props.color || '#547862' }} spin />;
+
+    return (
+        <LoaderContainer size={props.size}>
+            <Spin indicator={antIcon} />
+            <div>{props.message || 'Loading, please wait.'}</div>
+        </LoaderContainer>
+    )
+}
+
+export default Loader;
