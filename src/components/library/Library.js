d3d84af49e09b0fb306448d0a9b71035c5de4179
Mr. Jonah <jaitch@gmail.com>
2020-03-26 12:33:36 -0500

    DS Recommendations added

--- src/components/library/Library.js
+++ src/components/library/Library.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
-import { fetchUsersBooks, fetchUsersShelves, setBreadcrumbs } from '../../actions';
+import { fetchUsersBooks, fetchUsersShelves, setBreadcrumbs, fetchRecommendations } from '../../actions';
 import Header from '../common/Header';
 import SearchForm from '../search/SearchForm';
 import MyShelves from '../common/MyShelves';
@@ -17,12 +17,11 @@ const Library = props => {
 		Event('Library', 'User library loaded', 'LIBRARY');
 		PageView();
 	}, []);
-
+	
 	const fullName = localStorage.getItem('full_name').split(' ')[0];
-	const userBooks = props.userBooks.filter(item => item);
 	const toBeRead = props.userBooks.filter(item => item.readingStatus === 1);
 	const inProgress = props.userBooks.filter(item => item.readingStatus === 2);
-	
+
 	return (
 		<LibraryContainer>
 			<Header history={props.history} />
@@ -41,8 +40,13 @@ const Library = props => {
 						<>
 							<StatusShelfCarousel title="In progress" display="card" bookList={inProgress} link="/shelf/inprogress" breadcrumbs={[{ label: "In progress", path: "/shelf/inprogress" }, { label: "Book details", path: null }]} history={props.history} />
 							<StatusShelfCarousel title="To be read" display="card" bookList={toBeRead} breadcrumbs={[{ label: "To be read", path: "/shelf/toberead" }, { label: "Book details", path: null }]} link="/shelf/toberead" history={props.history} />
-							{/* <StatusShelfCarousel title="Finished" display="card" bookList={finished} breadcrumbs={[{ label: "Finished", path: "/shelf/finished" }, { label: "Book details", path: null }]} link="/shelf/finished" history={props.history} /> */}
-							<StatusShelfCarousel title="Recommendations" display="carousel" bookList={userBooks} breadcrumbs={[{ label: "Recommendations", path: "/shelf/recommendations" }, { label: "Book details", path: null }]} history={props.history} />
+							{
+								props.recommendations &&
+								props.recommendations.books &&
+								<StatusShelfCarousel title="Recommendations" display="carousel" bookList={props.recommendations.books} breadcrumbs={[{ label: "Recommendations", path: "/shelf/recommendations" }, { label: "Book details", path: null }]} history={props.history} />
+							}
+							
+							
 						</>
 					)}	
 				</div>
@@ -56,8 +60,9 @@ const Library = props => {
 const mapStateToProps = state => {
 	return {
 		userBooks: state.library.userBooks,
-		userShelves: state.library.userShelves
+		userShelves: state.library.userShelves,
+		recommendations: state.recommendations.recommendations
 	};
 };
 
-export default connect(mapStateToProps, { fetchUsersBooks, fetchUsersShelves, setBreadcrumbs })(Library);
+export default connect(mapStateToProps, { fetchUsersBooks, fetchUsersShelves, setBreadcrumbs, fetchRecommendations })(Library);
